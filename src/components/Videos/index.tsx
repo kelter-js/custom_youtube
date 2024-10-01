3476a71756153dc5b53477da1d19792c2fd5d345
kelter-js <spawnkillerqw@gmail.com>
2024-10-01 14:57:25 +0500

    feat: implemented Videos component

---
+++ src/components/Videos/index.tsx
@@ -0,0 +1,38 @@
+import { FC, useMemo, Fragment } from "react";
+import { Box, Skeleton } from "@mui/material";
+
+import { VideosContainer } from "./Videos.styled";
+import { VideosProps } from "./types";
+import VideoCard from "../VideoCard";
+import ChannelCard from "../ChannelCard";
+
+const Videos: FC<VideosProps> = ({ videos, sx = {} }) => {
+  if (!videos?.length) {
+    return <Skeleton animation="wave" sx={{ height: "100%", width: "100%" }} />;
+  }
+
+  const videosList = useMemo(
+    () =>
+      videos.map((video, ind) => (
+        <Fragment
+          key={
+            video.id.videoId ??
+            video?.snippet?.channelId ??
+            video?.statistics?.subscriberCount
+          }
+        >
+          {video.id.videoId && (
+            <Box key={ind}>
+              <VideoCard video={video} />
+              {video.id.channelId && <ChannelCard channelDetail={video} />}
+            </Box>
+          )}
+        </Fragment>
+      )),
+    [videos]
+  );
+
+  return <VideosContainer sx={{ ...sx }}>{videosList}</VideosContainer>;
+};
+
+export default Videos;
