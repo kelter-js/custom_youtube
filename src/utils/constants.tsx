48b20b69f6cd1da36a5739f63af408c481a05654
kelter-js <spawnkillerqw@gmail.com>
2024-10-01 14:55:08 +0500

    feat: added standalone constants file

---
+++ src/utils/constants.tsx
@@ -0,0 +1,42 @@
+import {
+  TheaterComedy,
+  FitnessCenter,
+  LiveTv,
+  School,
+  FaceRetouchingNatural,
+  Checkroom,
+  GraphicEq,
+  SportsEsports,
+  OndemandVideo,
+  Code,
+  Home,
+  MusicNote,
+} from "@mui/icons-material";
+
+export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+
+export const categories = [
+  { name: "New", icon: <Home /> },
+  { name: "Coding", icon: <Code /> },
+  { name: "ReactJS", icon: <Code /> },
+  { name: "NextJS", icon: <Code /> },
+  { name: "Music", icon: <MusicNote /> },
+  { name: "Education", icon: <School /> },
+  { name: "Podcast", icon: <GraphicEq /> },
+  { name: "Movie", icon: <OndemandVideo /> },
+  { name: "Gaming", icon: <SportsEsports /> },
+  { name: "Live", icon: <LiveTv /> },
+  { name: "Sport", icon: <FitnessCenter /> },
+  { name: "Fashion", icon: <Checkroom /> },
+  { name: "Beauty", icon: <FaceRetouchingNatural /> },
+  { name: "Comedy", icon: <TheaterComedy /> },
+  { name: "Gym", icon: <FitnessCenter /> },
+];
+
+export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
+export const demoChannelUrl = "/channel/UC_pPmy6BQq-SQnek6gnxAiA";
+export const demoVideoUrl = "/watch?v=dQw4w9WgXcQ";
+export const demoChannelTitle = "Template channel title";
+export const demoVideoTitle = "Template title";
+export const demoProfilePicture =
+  "http://dergipark.org.tr/assets/app/images/buddy_sample.png";
